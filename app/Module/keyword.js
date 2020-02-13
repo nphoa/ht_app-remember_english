5739ebde9c604b997704808a10c193fb73b8a9ca
unknown <nphoa7777@gmail.com>
2020-02-13 22:49:19 +0700

    'commit3'

--- app/Module/keyword.js
+++ app/Module/keyword.js
@@ -1,3 +1,31 @@
 var keywordModule = (function () {
-
+    var data = [
+        {
+            'id'       :1,
+            'keyword'  : 'english',
+            'vietnames': 'tiếng anh'
+        },
+        {
+            'id'       :2,
+            'keyword'  : 'account',
+            'vietnames': 'tài khoản'
+        },
+        {
+            'id'       :3,
+            'keyword'  : 'animal',
+            'vietnames': 'động vật'
+        },
+        {
+            'id'       :4,
+            'keyword'  : 'amy',
+            'vietnames': 'quân đội'
+        }
+    ];
+    function randomKeyWord(count) {
+        
+    }
+    return {
+        data:data,
+        randomKeyWord:randomKeyWord
+    }
 })();
--- app/index.js
+++ app/index.js
@@ -1,5 +1,17 @@
-$(document).ready(function () {
-    $("#randomKeyword").on('click',function () {
-        $("div#infoKeywords").css('display','');
+(function($){
+    $(document).ready(function () {
+        $("#randomKeyword").on('click',function () {
+            commonModule.getRandomIntInclusive(1,3);
+            let keywords = keywordModule.data;
+            for (let i = 0; i < keywords.length; i++) {
+                console.log(keywords[i]);
+                let input_english = `<input type="text" class="form-control" value="${keywords[i].keyword}" disabled> <br>`;
+                $("div#english").append(input_english);
+                let input_vietnamese = `<input type="text" class="form-control" value="${keywords[i].vietnames}" disabled> <br>`;
+                $("div#vietnamese").append(input_vietnamese);
+            }
+            $("div#infoKeywords").css('display','');
+        });
+
     });
-});
+})(jQuery);
